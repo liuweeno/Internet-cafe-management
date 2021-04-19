b0c82223cd2896a75c93d9b55826449fc5c05d54
Wair Chan <y.yw@live.com>
2021-04-19 20:00:15 +0800

    Unable to support Linux, rollback.

--- requirements.h
+++ requirements.h
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef _WIN32
 #include <conio.h>
-#else
-#include "getch_linux.h"
-#endif
 #include <unistd.h>
 #include <stdbool.h>
 #include <time.h>
