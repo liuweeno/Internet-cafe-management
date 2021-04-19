c3fa630d819db7773cadfdc704d9870bcee8ab2f
Wair Chan <y.yw@live.com>
2021-04-19 19:52:09 +0800

    getch() support Linux

--- requirements.h
+++ requirements.h
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
+#ifdef _WIN32
 #include <conio.h>
+#else
+#include "getch_linux.h"
+#endif
 #include <unistd.h>
 #include <stdbool.h>
 #include <time.h>
